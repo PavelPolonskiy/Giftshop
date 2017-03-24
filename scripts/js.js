dd3b461e8544abb148157bbc27b689713450978e
pavel <pavelpolonskiy@mail.ru>
2017-03-24 10:40:29 +0300

    sass

--- scripts/js.js
+++ scripts/js.js
@@ -1,1 +1,14 @@
+(function() {
+	var slideWidth = 300;
+	var slidesCount = $('.featured').length;
+	var sliderUl = $('.slider-ul');
+	sliderUl.css('transform', '0px');
 
+	$('.right-a').on('click', function() {
+		var curSlide = parseInt( sliderUl.css('transform') );
+		sliderUl.css('transform', curSlide - slideWidth + 'px');
+		if (curSlide < -((slideWidth - 2) * slidesCount)) {
+			sliderUl.css('transform', '0px');
+		}
+	});
+})();
